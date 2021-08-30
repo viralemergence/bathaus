e7667b650e8d4e1c9033e44fe5d9d9cba1211ae5
Daniel Becker <dbecker@uga.edu>
2021-08-30 13:27:43 -0500

    merged phylo with viruses

--- scripts/02_virus to phylo.R
+++ scripts/02_virus to phylo.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(ape)
 library(Hmisc)
 library(plyr)
+library(easyPubMed)
 
 ## load species-level virus data
 setwd("~/Desktop/bathaus/virus data")
@@ -65,11 +66,82 @@ species$species=revalue(species$species,
                           "Hypsugo alaschanicus"="Pipistrellus alaschanicus",
                           "Hypsugo pulveratus"="Pipistrellus pulveratus",
                           "Hypsugo savii"="Pipistrellus savii",
-                          "Laephotis capensis"="Eptesicus capensis",
+                          "Laephotis capensis"="Neoromicia capensis",
                           "Lissonycteris angolensis"="Myonycteris angolensis",
-                          "Macronycteris commersoni"=""
-                          
-                          
-                          
-                          
-                          ))
+                          "Macronycteris commersoni"="Hipposideros commersoni",
+                          "Macronycteris gigas"="Hipposideros gigas",
+                          "Macronycteris vittata"="Hipposideros vittatus",
+                          "Miniopterus africanus"="Miniopterus inflatus",
+                          "Miniopterus orianae"="Miniopterus oceanensis",
+                          "Molossus ater"="Molossus rufus",
+                          "Myotis myotis/blythii"="Myotis myotis",
+                          "Myotis oxygnathus"="Myotis blythii",
+                          "Myotis ricketti"="Myotis pilosus",
+                          "Neoromicia brunneus"="Neoromicia brunnea",
+                          "Nyctalus velutinus"="Nyctalus plancyi",
+                          "Parahypsugo crassulus"="Pipistrellus crassulus",
+                          "Perimyotis subflavus"="Pipistrellus subflavus",
+                          "Plecotus gaisleri"="Plecotus teneriffae",
+                          "Pteronotus alitonus"="Pteronotus parnellii",
+                          "Pteronotus rubiginosus"="Pteronotus parnellii",
+                          "Rhinolophus blythi"="Rhinolophus lepidus",
+                          #"Rhinolophus cornutus"="",
+                          "Rhinolophus hildebrandti"="Rhinolophus hildebrandtii",
+                          "Rhinolophus lobatus"="Rhinolophus landeri",
+                          "Rhinolophus monoceros"="Rhinolophus pusillus",
+                          "Rhinolophus rhodesiae"="Rhinolophus simulator"))
+
+## recheck missing
+setdiff(species$species,taxa$species)
+
+## remove missing
+species=species[!species$species%in%setdiff(species$species,taxa$species),]
+
+## do we have duplicate species names after taxonomic matching?
+table(table(species$species)>1)
+
+## aggregate
+species=aggregate(virus~species,data=species,sum)
+
+## clean taxonomy file
+taxa=taxa[c("species","gen","fam","clade","MSW3_sciName_matched","tip")]
+
+## merge with species
+data=merge(taxa,species,by="species",all.x=T)
+
+## if no viruses, 0 viruses
+data$virus=ifelse(is.na(data$virus),0,data$virus)
+
+## collect any citations per bat species
+cites=c()
+for(i in 1:length(data$tip)) {
+  
+  counts=as.numeric(as.character(get_pubmed_ids(gsub('_','-',data$tip[i]))$Count))
+  cites[i]=counts
+  print(paste(i,"/",nrow(data)))
+}
+
+## virus-related citations per bat species
+vcites=c()
+for(i in 1:length(data$tip)) {
+  
+  x=gsub('_','-',data$tip[i])
+  x=paste("(",x,")",sep="")
+  x=paste(x,"AND (virus OR viral)")
+  counts=as.numeric(as.character(get_pubmed_ids(x)$Count))
+  vcites[i]=counts
+  print(paste(i,"/",nrow(data)))
+}
+
+## compile all citations
+cdata=data.frame(tip=data$tip,
+                 cites=cites,
+                 vcites=vcites)
+
+## merge
+data=merge(data,cdata,by='tip')
+
+## clean
+rm(cites,vcites,x,cdata,i,counts)
+
+## write flat file
